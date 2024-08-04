2b84bac4c444e9283205e6562d042477bb153349
cha777 <chathuranga_wm@yahoo.com>
2024-08-05 00:41:19 +0530

    PWA support

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import { App } from './app';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <HelmetProvider>
-    <BrowserRouter>
-      <Suspense>
-        <App />
-      </Suspense>
-    </BrowserRouter>
-  </HelmetProvider>
+  <BrowserRouter>
+    <Suspense>
+      <App />
+    </Suspense>
+  </BrowserRouter>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,51 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'BMC Commissioner',
+        short_name: 'BMC Commissioner',
+        description: 'Bandula Metal Crusher Commission Calculator',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        orientation: 'portrait',
+        display: 'fullscreen',
+        background_color: '#0c0a09',
+        theme_color: '#0c0a09',
+        dir: 'ltr',
+        lang: 'en',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
